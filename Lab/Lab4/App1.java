96e2439effefd24ac5f8c7effe5308494ebdf4b3
Perpelici Alexandra <alexandraperpelici1@gmail.com>
2024-04-15 00:44:07 +0300

    lab4

---
+++ Lab/Lab4/App1.java
@@ -0,0 +1,14 @@
+package Lab.Lab4;
+
+public class App1 {
+    public static void main(String[] args) {
+        Integer monitor1 = new Integer(1);
+        Integer monitor2 = new Integer(1);
+
+
+        new ExecutionThreadApp1(monitor1, null,4, 2, 4).start();
+        new ExecutionThreadApp1(monitor1,monitor2,3, 3, 6).start();
+        new ExecutionThreadApp1(null,monitor2, 5, 2,5).start();
+
+    }
+}
